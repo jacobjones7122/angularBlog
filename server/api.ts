b8c718ff6a5cf1f5b3a4c25cff33c23fbda8096c
Jacob-Jones <jacobjones7122@gmail.com>
2017-11-16 11:10:49 -0500

    sendgrid

--- server/api.ts
+++ server/api.ts
@@ -2,13 +2,13 @@ import * as express from 'express';
 import posts from './controllers/posts.ctrl';
 import users from './controllers/users.ctrl';
 import categories from './controllers/categories.ctrl';
-import contactForm from './controllers/contactform.ctrl';
+import contactForms from './controllers/contactforms.ctrl';
 
 let router = express.Router();
 
 router.use('/posts', posts);
 router.use('/users', users);
 router.use('/categories', categories);
-router.use('/contactform', contactForm);
+router.use('/contactforms', contactForms);
 
 export default router;
---
+++ server/controllers/contactforms.ctrl.js
@@ -0,0 +1,16 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const email_svc_1 = require("../services/email.svc");
+const express_1 = require("express");
+const router = express_1.Router();
+router.post('/', (req, res) => {
+    console.log('In the contactForm.ctrl');
+    console.log(req.body);
+    email_svc_1.sendEmail('jacobjones7122@gmail.com', req.body.from, req.body.subject, req.body.message)
+        .then((response) => {
+        res.sendStatus(204);
+    }).catch((error) => {
+        res.sendStatus(500);
+    });
+});
+exports.default = router;
